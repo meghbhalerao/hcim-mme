75e178fef2144bed72e59c3064276ff2986cd0ef
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-06-27 11:21:57 +0530

    some more

--- misc-scripts/acc_from_cf.py
+++ misc-scripts/acc_from_cf.py
@@ -1,3 +1,27 @@
 import numpy as np
+
+# Making the list of classes
+class_num_actual = "sketch_num_actual_pc.txt"
+f_act = open(class_num_actual)
+f_act = f_act.read().split("\n")
+class_num_actual = []
+for item in f_act:
+    if item != '':
+        class_num_actual.append(int(item))
+
+class_num_actual = np.array(class_num_actual)
+
 cf = np.load("cf_target.npy")
-print(np.trace(cf)/np.sum(cf))
+sum_row = np.sum(cf,axis=0)
+i=0
+acc_list = []
+summ = 0
+for i in range(126):
+    acc_list.append(cf[i,i]/sum_row[i])
+    summ = summ + abs(class_num_actual[i] - sum_row[i])
+    print(abs(class_num_actual[i] - sum_row[i]))
+acc_list = np.array(acc_list)
+
+print(np.mean(acc_list))
+print(summ/np.sum(cf))
+print(np.trace(cf)/np.sum(cf))    
--- misc-scripts/plot.py
+++ misc-scripts/plot.py
@@ -4,8 +4,10 @@ import re
 import os
 
 # class list dirty strings
-class_num_actual = "sketch_num_actual_pc.txt"
+class_num_actual = "real_num_actual_pc.txt"
 class_num_pred = "sketch_num_pred_pc.txt"
+
+
 # Making the list of classes
 f_act = open(class_num_actual)
 f_act = f_act.read().split("\n")
@@ -13,6 +15,7 @@ class_num_actual = []
 for item in f_act:
     if item != '':
         class_num_actual.append(int(item))
+
 
 # Making the list of classes
 f_act = open(class_num_pred)
@@ -23,23 +26,19 @@ for item in f_act:
         class_num_pred.append(int(item))
 class_num_actual = np.array(class_num_actual)
 class_num_pred = np.array(class_num_pred)
+
+
 # sorting both the arrays accoring to the actual number of exmaples per class
-
+print(abs(np.sum(class_num_pred-class_num_actual))/sum(class_num_actual))
 arr1inds = class_num_actual.argsort()
-class_num_actual = class_num_actual[arr1inds[::-1]]
-class_num_pred = class_num_pred[arr1inds[::-1]]
+class_num_actual = class_num_actual[arr1inds]
+class_num_pred = class_num_pred[arr1inds]
 
-class_num_actual = np.flip(class_num_actual)
-class_num_pred = np.flip(class_num_pred)
 
-print(sum(class_num_actual))
-print(sum(class_num_pred))
-
-plt.plot(class_num_pred, label = "predicted distribution by class of target examples")
-plt.plot(class_num_actual, label = "actual distribution by class of target examples")
+plt.plot(class_num_pred, label = "predicted target distribution by class of target examples")
+plt.plot(class_num_actual, label = "actual source distribution by class of target examples")
 plt.legend()
-plt.title("Sorted in ascending order according to the actual class distribution")
+plt.title("Distributions")
 plt.xlabel('Class')
 plt.ylabel('# examples in the class')
 plt.show()
-
