be4c16a0112d359755ee50bb638b2ab96fdc8a4a
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-06-29 20:48:32 +0530

    changes

--- misc-scripts/plotting/plot.py
+++ misc-scripts/plotting/plot.py
@@ -4,17 +4,18 @@ import re
 import os
 
 # class list dirty strings
-class_num_actual = "real_num_actual_pc.txt"
+class_num_actual_source = "real_num_actual_pc.txt"
+class_num_actual_target = "sketch_num_actual_pc.txt"
 class_num_pred = "sketch_num_pred_pc.txt"
 
 
-# Making the list of classes
-f_act = open(class_num_actual)
+# Making the list of classes and their number of samples
+f_act = open(class_num_actual_source)
 f_act = f_act.read().split("\n")
-class_num_actual = []
+class_num_actual_source = []
 for item in f_act:
     if item != '':
-        class_num_actual.append(int(item))
+        class_num_actual_source.append(int(item))
 
 
 # Making the list of classes
@@ -24,19 +25,32 @@ class_num_pred = []
 for item in f_act:
     if item != '':
         class_num_pred.append(int(item))
-class_num_actual = np.array(class_num_actual)
-class_num_pred = np.array(class_num_pred)
 
+
+# Making the list of classes
+f_act = open(class_num_actual_target)
+f_act = f_act.read().split("\n")
+class_num_actual_target = []
+for item in f_act:
+    if item != '':
+        class_num_actual_target.append(int(item))
+
+
+class_num_actual_source = np.array(class_num_actual_source)
+class_num_pred = np.array(class_num_pred)
+class_num_actual_target = np.array(class_num_actual_target)
 
 # sorting both the arrays accoring to the actual number of exmaples per class
-print(abs(np.sum(class_num_pred-class_num_actual))/sum(class_num_actual))
-arr1inds = class_num_actual.argsort()
-class_num_actual = class_num_actual[arr1inds]
+#print(np.sum(abs(class_num_pred-class_num_actual))/sum(class_num_pred))
+
+arr1inds = class_num_actual_target.argsort()
+class_num_actual_target = class_num_actual_target[arr1inds]
 class_num_pred = class_num_pred[arr1inds]
+class_num_actual_source = class_num_actual_source[arr1inds]
 
-
-plt.plot(class_num_pred, label = "predicted target distribution by class of target examples")
-plt.plot(class_num_actual, label = "actual source distribution by class of target examples")
+plt.plot(class_num_pred, label = "predicted class target distribution")
+plt.plot(class_num_actual_source, label = "actual class source distribution")
+plt.plot(class_num_actual_target, label="actual class target distribution")
 plt.legend()
 plt.title("Distributions")
 plt.xlabel('Class')
