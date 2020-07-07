5ff9ac0d8be61c1f9489be5320c0fa5ce83cf785
Megh Bhalerao Megh.Bhalerao@Pennmedicine.upenn.edu <megh.bhalerao@pennmedicine.upenn.edu>
2020-07-07 04:31:23 -0400

    making word 2 vec

--- misc-scripts/word_embeddings.py
+++ misc-scripts/word_embeddings.py
@@ -2,6 +2,7 @@ from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
 import re
 import numpy as np
+from scipy import spatial 
 
 # Setting which embedding and what dimension of the embedding we want
 dataset = "multi"
@@ -26,7 +27,6 @@ for item in my_file:
 
 del[class_list[len(class_list)-1]]
 
-
 # Loading the specific word embedding model
 if embed == 'w2v':
     filename = '../../../model/%s-pret/%s'%(embed,embed_model)
@@ -43,25 +43,40 @@ elif embed == 'fasttext':
 def find_closest_embeddings(embedding):
     return sorted(embeddings_dict.keys(), key=lambda word: spatial.distance.euclidean(embeddings_dict[word], embedding))
 
-# Loading the model in w2v format for harmonization
+# Loading the model in w2v format for harmonization across different models
 model = KeyedVectors.load_word2vec_format(filename, binary=binary)
-words = list(model.wv.vocab)
-print(model.wv)
+word_list = list(model.wv.vocab)
+# Creating a dict of words and the corresponding word vectors
+embeddings_dict = {}
+for word in word_list:
+    embeddings_dict[word] = model[word]
+# creating a class list of original and fake labels
+class_dict = {}
+for class_ in class_list:
+    class_dict[class_] = class_.lower()
+# Handline some special cases here for words which are not present in the dict
+class_dict['bottlecap'] = 'bottle_cap'
+class_dict['teddy-bear'] = 'teddy_bear'
+
 
 num_class =  len(class_list)
 a = np.zeros((num_class,dim),dtype = float)
 i = 0
 
-
-for item in class_list:
-    item = str(item)
-    item = item.lower()
-    vec  = model[item]
-    a[i,:] = vec
-    print(vec[0:10])
-    i = i + 1 
-    print(i)
-    
-np.save('%s_%s.npy'%(dataset,embed_model),a)
+total=[]
+print(len(embeddings_dict))
+for og_labels in class_dict.keys():
+    if og_labels in embeddings_dict:
+        total.append(embeddings_dict[og_labels]/np.linalg.norm(embeddings_dict[og_labels]))
+        print("computed direct label")
+    else:
+        mini_labels=class_dict[og_labels].split('_')
+        print(mini_labels)
+        final_embedding=np.sum([embeddings_dict[xlabel] for xlabel in mini_labels],axis=0)
+        nearest_label=find_closest_embeddings(final_embedding)[0]
+        total.append(embeddings_dict[nearest_label]/np.linalg.norm(embeddings_dict[nearest_label]))
+        print("computed nearest neighbour")
+    	
+np.save('%s_%s.npy'%(dataset,embed_model),total)
 
 
