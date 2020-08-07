7b9552872a0a609c229a34670c8a4d06bd7e078b
megh <megh.bhalerao@gmail.com>
2020-08-07 05:26:22 -0400

    added function for class imbalance

--- misc-scripts/create_imbalance.py
+++ misc-scripts/create_imbalance.py
@@ -3,6 +3,7 @@ import os
 dataset = "office"
 domain = "dslr"
 imbalance_type = "exp"
+
 if dataset == "office":
     data_path = "../data/%s/%s/images/"%(dataset,domain)
 
@@ -11,4 +12,20 @@ for class_ in os.listdir(data_path):
     num_examples.append(len(os.listdir(os.path.join(data_path,class_))))
 
 num_examples.sort(reverse = True)
-print(num_examples)
+
+
+def get_img_num_per_cls(cls_num, imb_type, imb_factor):
+    img_max = len(data) / cls_num
+    img_num_per_cls = []
+    if imb_type == 'exp':
+        for cls_idx in range(cls_num):
+            num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
+            img_num_per_cls.append(int(num))
+    elif imb_type == 'step':
+        for cls_idx in range(cls_num // 2):
+            img_num_per_cls.append(int(img_max))
+        for cls_idx in range(cls_num // 2):
+            img_num_per_cls.append(int(img_max * imb_factor))
+    else:
+        img_num_per_cls.extend([int(img_max)] * cls_num)
+    return img_num_per_cls
